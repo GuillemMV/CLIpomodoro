9293b1f317cefedc26ca0e819a75cb5dcd1e014b
guillemmv <56261354+GuillemMV@users.noreply.github.com>
2022-12-17 16:54:52 +0100

    Add files via upload

--- cli.py
+++ cli.py
@@ -1,6 +1,13 @@
+"""
+Author: Guillem Magriña Vernet
+Date: December, 2022
+"""
+
 import typer
 from tqdm import tqdm
 from time import sleep
+from datetime import datetime
+import os
 
 app = typer.Typer()
 WorkMinutes = 0
@@ -8,29 +15,41 @@ RelaxMinutes = 0
 
 def workTime():
     seconds = int(WorkMinutes*60)
-    for i in tqdm(range(seconds),desc="Work time",bar_format="{l_bar}{bar}"):
-        sleep(1)
+    for _ in tqdm(range(seconds),desc="WORK🔨",bar_format="{l_bar}{bar}", colour="red"):
+        current = datetime.now()
+        time = current.strftime("%H:%M:%S")
 
-def relaxTime():
-    seconds = int(RelaxMinutes*60)
-    for i in tqdm(range(seconds),desc="Relax Time",bar_format="{l_bar}{bar}"):
+        print(time)
         sleep(1)
+        os.system("clear")
+
+def breakTime():
+    seconds = int(RelaxMinutes*60)
+    for _ in tqdm(range(seconds),desc="BREAK☕",bar_format="{l_bar}{bar}", colour="green"):
+        current = datetime.now()
+        time = current.strftime("%H:%M:%S")
+
+        print(time)
+        sleep(1)
+        os.system("clear")
 
 def start():
     try:
         while True:
             workTime()
-            relaxTime()
+            breakTime()
     except KeyboardInterrupt as stop:
         print("You finished the pomodoro! Bye!")
 
 @app.command()
 def setTime(worktime:int,relaxtime:int):
-    global RelaxMinutes, WorkMinutes
-    RelaxMinutes = relaxtime
-    WorkMinutes = worktime
-    typer.echo("Time set correctly!")
-    start()
-
+    try:
+        global RelaxMinutes, RelaxMinutes
+        RelaxMinutes = relaxtime
+        WorkMinutes = worktime
+        typer.echo("Time set correctly!")
+        start()
+    except Exception as exc:
+        print("ERROR: ",exc)
 if __name__ == "__main__":
     app()
